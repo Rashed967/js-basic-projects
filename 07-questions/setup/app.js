7d357a83491d2d25df5f1016b0330bd04de87016
Rashed967 <rashedhasan967@gmail.com>
2023-03-17 19:53:54 +0600

    question part done

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
+const questions = document.querySelectorAll('.question')
+
+// loop on questions 
+questions.forEach(question => {
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click', () => {
+        question.classList.toggle('show-text')
+        questions.forEach(item => {
+            (item !== question) ? item.classList.remove('show-text') : ''
+        })
+    })
+})
+
+
+
+
 // traversing the dom
+// // get all buttons 
+// const btns = document.querySelectorAll('.question-btn')
+
+// // run loop on qustions 
+// btns.forEach( (btn) =>{
+//     btn.addEventListener('click', (e) =>{
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle('show-text')
+//     })
+// })
